a0c2034027e0e1a6a678e7c7f0b12323df03f791
tktls7374 <147792442+tktls7374@users.noreply.github.com>
2023-11-24 14:25:09 +0900

    Add files via upload

---
+++ num3/comprehensive_test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    
+    for (int i = 1; i < 25; i++)
+    {
+        if (i)
+        {
+            /* code */
+        }
+        
+    }
+    
+    while (i<=50)
+    {
+        printf("%d\n", i);        
+        i++;
+    }
+    
+    
+    return 0;
+
+}
